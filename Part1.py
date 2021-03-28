f0d283f9b9ad25ad37fc2fac00036caa464883ad
Ayush12062000 <kaayush112@gmail.com>
2021-03-28 15:03:27 +0530

    Modification required

--- Part1.py
+++ Part1.py
@@ -31,13 +31,13 @@ training_set = train_datagen.flow_from_directory(
     'Dataset/train',
     target_size=(64,64),
     batch_size=10,
-    class_mode='input')
+    class_mode='binary')
 
 val_set = val_datagen.flow_from_directory(
     'Dataset/validation',
     target_size=(64,64),
     batch_size=10,
-    class_mode='input')
+    class_mode='binary')
 
 # %%
 model = classifier_model()
@@ -61,8 +61,43 @@ with tf.device('/GPU:0'):
         epochs=100, 
         verbose=1,
         validation_data = val_set,
-        #validation_steps=266//10,
-        #callbacks=[callback]
+        validation_steps=266//10,
+        callbacks=[callback]
     )
-    
+
 # %%
+import matplotlib.pyplot as plt
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+import numpy as np
+
+#%%
+path = 'kiwi.jpg'
+img_original = load_img(path)
+img = load_img(path, target_size = (64,64))
+img_tensor = img_to_array(img)
+img_tensor = np.expand_dims(img_tensor, axis = 0)
+img_tensor/=255.0
+
+# %%
+
+pred = model.predict(img_tensor)
+print(pred)
+
+# %%
+if pred<.5: str = '--------------Orange--------------'
+else: str = '--------------Non Orange--------------'
+
+# %%
+plt.imshow(img_original)
+plt.axis('off')
+plt.title(str)
+plt.show()
+
+# %%
+''' 
+okay so far my model is baised. Since i just traing my model on the
+images of the orange. It is classifing every image as orange.
+need to add another class here stating that those are not oranges.
+
+''' 
