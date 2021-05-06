1db129364400cc0bf24c41ae1ef93eb6bbdc1a2e
Ayush12062000 <kaayush112@gmail.com>
2021-05-06 12:48:15 +0530

    Helper code for dataset creation

---
+++ Image Segmentaion/Folder_Creater.py
@@ -0,0 +1,22 @@
+#%%
+# importing os module
+import os
+
+# Directory
+directory = "xyz"
+
+# Parent Directory path
+parent_dir = "E:/Projects 6th SEM/Orange-Fruit-Recognition-Using-Image-Segmentation/Image Segmentaion/test_data/"
+
+
+for i in range(1,78):
+    path = os.path.join(parent_dir, directory+str(i))
+    
+    os.mkdir(path)
+    f1 = "images"
+    path1 = os.path.join(path,f1)
+    os.mkdir(path1)
+    print("Directory '% s' created" % directory)
+
+
+# %%
