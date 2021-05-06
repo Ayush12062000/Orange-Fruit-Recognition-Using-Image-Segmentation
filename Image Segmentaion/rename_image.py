aec6a17a1adb10c210274269776bc09052b7e145
Ayush12062000 <kaayush112@gmail.com>
2021-05-07 02:09:43 +0530

    Helper code for dataset creation

---
+++ Image Segmentaion/rename_image.py
@@ -0,0 +1,38 @@
+#%%
+import os
+
+#%%
+# for train data
+# os.chdir('E:/Projects 6th SEM/Orange Fruit Resources/train_data')
+
+# for test data
+os.chdir('E:/Projects 6th SEM/Orange Fruit Resources/test_data')
+
+
+# %%
+os.listdir()
+
+# %%
+
+# train data
+for i, filename in enumerate(os.listdir()):
+    x = os.path.join(filename+'/images')
+    for j in enumerate(os.listdir(x)):
+        #print(j[1])
+        
+        os.rename(os.path.join(x,j[1]), 
+                  os.path.join(x,filename + '.jpg'))
+    
+    
+    
+#%%
+
+# test data
+for i, filename in enumerate(os.listdir()):
+    x = os.path.join(filename+'/images')
+    for j in enumerate(os.listdir(x)):
+        #print(j[1])
+        
+        os.rename(os.path.join(x,j[1]), 
+                  os.path.join(x,filename + '.jpg'))
+# %%
